3b67f6802c142090b111c600efd9c5a4d98b9e38
Miki <alerpace@o2.pl>
2019-06-13 10:59:02 +0200

    Updates

---
+++ precache-manifest.2161718de84ecc2ba7aafde510498402.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76b89d67e65c8f31eeec1d9762b57df6",
+    "url": "/kursy-kryptowalut/index.html"
+  },
+  {
+    "revision": "faf2b71aa52e5cdb3a49",
+    "url": "/kursy-kryptowalut/static/css/main.f06945fc.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/kursy-kryptowalut/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "faf2b71aa52e5cdb3a49",
+    "url": "/kursy-kryptowalut/static/js/main.0349fc05.chunk.js"
+  },
+  {
+    "revision": "82402805be855f4f3a74",
+    "url": "/kursy-kryptowalut/static/js/runtime~main.4cfae9e2.js"
+  }
+]);
